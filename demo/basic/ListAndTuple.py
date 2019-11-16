1eab32fa1370cb5f3669c15e1be34dc3b12fa2f2
mylin95 <mylin95@126.com>
2019-11-16 20:04:09 +0800

    两种数据类型：list 和 tuple

--- demo/basic/ListAndTuple.py
+++ demo/basic/ListAndTuple.py
@@ -30,9 +30,32 @@ print(classmates)
 classmates[1] = 'Amy'
 print(classmates)
 
+# list里面的元素的数据类型也可以不同
+L = ['Apple', 123, True]
+print(L)
+# list元素也可以是另一个list
+s = ['python', 'java', ['asp', 'php'], 'scheme']
+print(s)
+# list的元素包含list，拆开写更优雅
+p = ['asp', 'php']
+s = ['python', 'java', p, 'scheme']
+print(s)
 
+# list的长度
+print(len(s))
 
-
+# 另一种有序列表叫元组：tuple
+# tuple类似list，不过它是只读的，一旦初始化不可更改
+classmates = ('Michael', 'Bob', "Tracy")
+# classmates[1] = 'Amy' # 不可赋值，报错
+print(classmates)
+t = ()      # 定义空的tuple
+t = (1)     # 定义只有一个元素1的元组(tuple)   ——有歧义
+t = (1, )   # 定义只有一个元素的元组(tuple)
+t = ('a', 'b', ['A', 'B'])  # 定义只读的二维数组
+t[2][0] = 'X'               # 内嵌一个list，它的值可以更改
+t[2][1] = 'Y'
+print(t)
 
 
 
